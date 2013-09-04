3a6098139e73b58ed049b31ddcb7bd2e4b8fe0fc
Shigeki Morimoto <github@morimo.to>
2013-09-04 11:08:52 +0900

    add DESCRIPTION.

--- lib/Text/AvoidBug/Apple/CoreText.pm
+++ lib/Text/AvoidBug/Apple/CoreText.pm
@@ -85,6 +85,10 @@ my $filtered = Text::AvoidBug::Apple::CoreText::filter($src);
 Text::AvoidBug::Apple::CoreText is filter that add 'left to right maker' to avoid apple CoreText bug.
 
 Apple CoreText has bug.
+There is a bug related to right-to-left text processing.
+There are too many patterns that trigger the bug, and addressing them individually might leave some patterns out, so instead I have chosen to insert a "left-to-right mark" right after any right-to-left character.
+Doing so might affect Arabic text and make it display improperly, sorry about that!
+{Arabic A}{Arabic B}{Arabic C}-=! should usually be displayed as {Arabic C}{Arabic B}{Arabic A}!=-, but will instead result in {Arabic C}{Arabic B}{Arabic A}-=!.
 
 =head1 LICENSE
 
